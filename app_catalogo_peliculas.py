9aebc8e94e8d5fd2352de0bb91760e23df83bd7c
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-17 19:49:06 +0200

    Creadas las funciones ejecutar_opcion y mostrar_menu

--- app_catalogo_peliculas.py
+++ app_catalogo_peliculas.py
@@ -1,5 +1,5 @@
 from servicio_peliculas import ServicioPeliculas
-
+from pelicula import Pelicula
 
 
 class AppCatalogoPeliculas:
@@ -16,7 +16,8 @@ class AppCatalogoPeliculas:
         self.servicio_peliculas.listar_peliculas()
         while not salir:
             try:
-                pass
+                opcion = self.mostrar_menu()
+                salir = self.ejecutar_opcion(opcion)
             except Exception  as e:
                 print(f"Ocurrio un error: {e}")
 
@@ -31,6 +32,31 @@ class AppCatalogoPeliculas:
               """)
         return int(input("Elige una opcion: "))
     
+    # Funcion que recoge la opcion y ejecuta el metodo correspondiente
+    def ejecutar_opcion(self,opcion):
+        if opcion == 1:
+            self.agregar_pelicula()
+        elif opcion == 2:
+            pass
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            print("Hasta la proxima !")
+            return True
+        else:
+            print(f"Opcion no valida: {opcion}")
+        return False
+    
+    # Funcion para agregar snacks 
+    def agregar_pelicula(self):
+        nombre = input("Nombre de la pelicula:   ")
+        nueva_pelicula = Pelicula(nombre)
+        self.servicio_peliculas.agregar_pelicula(nueva_pelicula)
+        print("Pelicula agregada correcttamente")
 
+# Programa principal
+if __name__ == "__main__":
+    app_catalogo_peliculas = AppCatalogoPeliculas()
+    app_catalogo_peliculas.catalogo_peliculas()
     
 
